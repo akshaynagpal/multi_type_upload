00e1ca0e357615351bd12cd18b1297976b5c7d3a
Akshay Nagpal <akshay2626@gmail.com>
2014-06-23 12:16:12 +0530

    updated src

--- image_upload.php
+++ image_upload.php
@@ -1,5 +1,5 @@
 <?php
-$allowedExts = array("jpg", "jpeg", "gif", "png");
+$allowedExts = array("jpg", "jpeg", "gif", "png","wmv");
 //$extension = pathinfo($_FILES['uploadimage']['name'], PATHINFO_EXTENSION);
 $temp = explode(".", $_FILES["uploadimage"]["name"]);
 $extension = end($temp);
@@ -7,7 +7,9 @@ $extension = end($temp);
 if (
 	$_FILES["uploadimage"]["type"] == "image/pjpeg"||
 	$_FILES["uploadimage"]["type"] == "image/gif"|| 
-	$_FILES["uploadimage"]["type"] == "image/jpeg"&&
+	$_FILES["uploadimage"]["type"] == "image/jpeg"||
+	$_FILES["uploadimage"]["type"] == "video/x-ms-wmv"
+	&&
 	in_array($extension, $allowedExts)
 	)
   	{
